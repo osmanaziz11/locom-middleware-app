687d2d052b99cdd303ee3f12f7a4605abe15cded
osman <osmanaziz012@gmail.com>
2023-09-25 04:57:27 +0500

    SMS API hooks integrated

--- routes/webhooks.py
+++ routes/webhooks.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 app = Blueprint('webhook', __name__)
 
@@ -6,3 +6,15 @@ app = Blueprint('webhook', __name__)
 @app.route("/webhook/", methods=['GET'])
 def index():
     return "Webhooks: Server is running."
+
+
+@app.route("/inbound-sms", methods=['GET', 'POST'])
+def inbound_sms():
+    print("asa")
+    if request.is_json:
+        print(request.get_json())
+    else:
+        data = dict(request.form) or dict(request.args)
+        print(data)
+
+    return ('', 204)
