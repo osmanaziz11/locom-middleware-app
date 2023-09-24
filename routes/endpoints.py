7714360125858152850dce53fa3a03e1da4c03a1
osman <osmanaziz012@gmail.com>
2023-09-24 19:17:38 +0500

    _

--- routes/endpoints.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-# Create a Blueprint instance
-app = Blueprint('endpoints', __name__)
-
-
-@app.route("/api/", methods=['GET'])
-def index():
-    return "API: Server is running."
--- routes/webhooks.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-# Create a Blueprint instance
-app = Blueprint('webhooks', __name__)
-
-
-@app.route("/webhook/", methods=['GET'])
-def index():
-    return "Webhook: Server is running."
