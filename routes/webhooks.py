4dfb9b6d53c057036bc14c6ffbb5c5a693e35f52
osman <osmanaziz012@gmail.com>
2023-10-04 01:59:28 +0500

    as

--- routes/webhooks.py
+++ routes/webhooks.py
@@ -6,15 +6,3 @@ app = Blueprint('webhook', __name__)
 @app.route("/webhook/", methods=['GET'])
 def index():
     return "Webhooks: Server is running."
-
-
-@app.route("/webhook/answer", methods=['GET', 'POST'])
-def abc():
-    print(request.json)
-    return jsonify([
-        {
-            "action": "talk",
-            "text": "Hello from voange"
-        }
-    ]
-    )
